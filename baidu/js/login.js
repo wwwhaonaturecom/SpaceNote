ee3fd7656bcb408dcaa574e6654b71e6ce4da1af
kagaya_server <kagaya85@foxmail.com>
2018-03-03 16:58:34 +0800

    修复缩放之后位置不对的问题，把头像中间正好对到了坐标位置

--- baidu/js/login.js
+++ baidu/js/login.js
@@ -12,3 +12,16 @@ function login_show() {
 		blurOverlay.style.visibility = 'hidden';
 	}
 }
+
+function login_hide(){
+		var loginWindow = document.getElementById("loginWindow");
+	var blurOverlay = document.getElementById("blurOverlay");
+	if(loginWindow.style.visibility == 'hidden'){
+		loginWindow.style.visibility = 'visible';
+		blurOverlay.style.visibility = 'visible';
+	}
+	else{
+		loginWindow.style.visibility = 'hidden';
+		blurOverlay.style.visibility = 'hidden';
+	}
+}
